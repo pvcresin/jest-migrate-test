1f486dd8b6d256d2aa9751d7c1576c8d0697ece6
pvcresin <pvcresin0730@gmail.com>
2022-01-18 21:50:27 +0900

    Add hook

--- src/sum.test.ts
+++ src/sum.test.ts
@@ -1,14 +1,58 @@
+import "./common";
+
+import { expect } from "chai";
+
 import sum from "./sum";
 
-// @ts-expect-error
-global.context = describe;
+// beforeAllの方が正しそう？ https://jestjs.io/ja/docs/setup-teardown#%E3%82%B9%E3%82%B3%E3%83%BC%E3%83%97
+beforeEach(() => {
+  console.log("beforeEach text");
+});
 
-// console.log(global.document.body);
+// afterAllの方が正しそう？
+afterEach(() => {
+  console.log("afterEach text");
+});
 
 describe("describe text", () => {
   context("context text", () => {
     it("it text", () => {
-      expect(sum(1, 2)).toBe(3);
+      // expect(sum(1, 2)).toBe(3); // Jest
+      expect(sum(1, 2)).to.eql(3); // Chai
     });
   });
 });
+
+// beforeAll(() => console.log("1 - beforeAll"));
+// afterAll(() => console.log("1 - afterAll"));
+
+// beforeEach(() => console.log("1 - beforeEach"));
+// afterEach(() => console.log("1 - afterEach"));
+
+// test("", () => console.log("1 - test"));
+
+// describe("Scoped / Nested block", () => {
+//   beforeAll(() => console.log("2 - beforeAll"));
+//   afterAll(() => console.log("2 - afterAll"));
+
+//   beforeEach(() => console.log("2 - beforeEach"));
+//   afterEach(() => console.log("2 - afterEach"));
+
+//   test("", () => console.log("2 - test"));
+// });
+
+// 1 - beforeAll
+
+// 1 - beforeEach
+// 1 -   #test
+// 1 - afterEach
+
+//   2 - beforeAll
+// 1 - beforeEach
+//   2 - beforeEach
+//   2 -   #test
+//   2 - afterEach
+// 1 - afterEach
+//   2 - afterAll
+
+// 1 - afterAll
